fbcd292af653ecf56f1d844b8a7ef81abb16311e
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-07-30 14:13:15 +0200

    changes

--- utility.h
+++ utility.h
@@ -1,9 +1,17 @@
 #ifndef UTILITY_UTILITY_H
 #define UTILITY_UTILITY_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "allocator.h"
 #include "bitwise.h"
 #include "checksum.h"
 #include "manager.h"
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // UTILITY_UTILITY_H
